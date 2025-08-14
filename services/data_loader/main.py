4341ffe28d50cbb67f34d3c6c2a5256d3edc6f3b
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-14 11:30:05 +0300

    in the main file i made the connection

--- services/data_loader/main.py
+++ services/data_loader/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from services.data_loader.db import DataLoader
+import os
 
 app = FastAPI()
 
-@app.get("/data")
-def get_data():
+loader = DataLoader(
+    host=os.environ.get("MYSQL_HOST", "localhost"),
+    user=os.environ.get("MYSQL_USER", "user"),
+    password=os.environ.get("MYSQL_PASSWORD", "password"),
+    database=os.environ.get("MYSQL_DATABASE", "mydb")
+)
 
-    return {"message": "placeholder"}
+
+@app.get("/data")
+def read_data():
+
+    return loader.get_all_data()
