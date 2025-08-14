6139e673294624d31fb369452c9ada43ca9edee7
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-14 11:13:36 +0300

    i started a connection to retriev from the establis

--- services/data_loader/db.py
+++ services/data_loader/db.py
@@ -2,8 +2,18 @@ import mysql.connector
 
 class DataLoader:
     def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1234",
+            database="mydb"
+        )
+
 
 
     def get_all_data(self):
-
-        return
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM data")
+        result = cursor.fetchall()
+        cursor.close()
+        return result
